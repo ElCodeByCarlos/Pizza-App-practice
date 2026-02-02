90e0f0abbcaa16eec399c5af42188ecd4482a1bc
carlo <carloswork067@hotmail.com>
2026-02-02 02:47:59 -0500

    First Stage of the App

---
+++ PizzaApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzaApp.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
